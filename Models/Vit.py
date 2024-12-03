dcaff1d445a43986ea711c0780f3934a5e4db273
avolius <30406685+slashm4n@users.noreply.github.com>
2024-12-03 20:33:31 +0100

    Update Vit.py

--- Models/Vit.py
+++ Models/Vit.py
@@ -5,20 +5,14 @@ from torch.utils.data import DataLoader
 from timm import create_model  # PyTorch image models library
 import csv
 import matplotlib.pyplot as plt
+import pandas as pd
 import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class ImageFolderWithPaths(datasets.ImageFolder):
-    def __getitem__(self, index):
-        # Original tuple: (image, label)
-        original_tuple = super().__getitem__(index)
-        # Add the file path
-        path = self.imgs[index][0]
-        return (*original_tuple, path)
 
-# Loads the data
-def data_loader(directory, batch_size):
+# Loads and normalizes the data and returns the data loaders
+def DataLoader(directory, batch_size):
     # Data Preprocessing
     transform = transforms.Compose([
         # transforms.Resize((224, 224)),
@@ -28,23 +22,23 @@ def data_loader(directory, batch_size):
 
     train_dir = directory + "\\train"
     val_dir = directory + "\\val"
-    val2_dir = directory + "\\test"
+    test_dir = directory + "\\test"
 
-    train_dataset = ImageFolderWithPaths(root=train_dir, transform=transform)
-    val_dataset = ImageFolderWithPaths(root=val_dir, transform=transform)
-    val2_dataset = ImageFolderWithPaths(root=val2_dir, transform=transform)
+    train_dataset = datasets.ImageFolder(root=train_dir, transform=transform)
+    val_dataset = datasets.ImageFolder(root=val_dir, transform=transform)
+    test_dataset = datasets.ImageFolder(root=test_dir, transform=transform)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    val2_loader = DataLoader(val2_dataset, batch_size=batch_size, shuffle=False)
-    return train_loader, val_loader, val2_loader
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return train_loader, val_loader, test_loader
 
 
 # Vision Transformer Model
 class ViTClassifier(nn.Module):
     def __init__(self, hidden_classes=3, num_classes=2):
         super(ViTClassifier, self).__init__()
-        self.model = create_model('vit_base_patch16_224', pretrained=True)  # loads a pre-trained ViT model
+        self.model = create_model('vit_base_patch16_224', pretrained=True)  # loads a pre-trained ViT model with 16x16 patches
         self.model.head = nn.Linear(self.model.head.in_features,
                                     hidden_classes)  # customize the model head to have 3 classes for the output (fire, smoke, non-fire)
         self.final = nn.Linear(hidden_classes,
@@ -54,13 +48,13 @@ class ViTClassifier(nn.Module):
         return self.final(self.model(x))
 
 
-# 4. Training Loop
-def train_epoch(model, loader, optimizer, criterion, device):
+# Training Loop
+def TrainEpoch(model, loader, optimizer, criterion, device):
     model.train()
     running_loss = 0.0
     i = 0
 
-    for batch_idx, (images, labels, paths) in enumerate(loader):
+    for images, labels in loader:
         images, labels = images.to(device), labels.to(device)
 
         optimizer.zero_grad()
@@ -71,124 +65,123 @@ def train_epoch(model, loader, optimizer, criterion, device):
         batch_size = images.size(0)
         running_loss += loss.item() * batch_size
 
-        i += batch_size
+        i += batch_size # used to find the loss per batch
         print("Running average train loss per image: " + str(running_loss / i))
-        break
 
     epoch_loss = running_loss / len(loader.dataset)
     return epoch_loss
 
 
 # Validation Loop
-def validate_epoch(model, loader, criterion, device, csv_file):
+def ValidateEpoch(model, loader, criterion, device):
     model.eval()
     running_loss = 0.0
     correct = 0
     total = 0
-
-    with open(csv_file, mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["id", "class", "Correct Label"]) #to remove last column when give to professors
-
     with torch.no_grad():
-        for batch_idx, (images, labels, paths) in enumerate(loader):
+        for j, (images, labels) in enumerate(loader, 0):
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             loss = criterion(outputs, labels)
             running_loss += loss.item() * images.size(0)
 
+            # does the predictions and sums the correct ones
             _, preds = torch.max(outputs, 1)
             correct += (preds == labels).sum().item()
             total += labels.size(0)
 
-            # Write each image's details to the CSV
-            for path, label, pred in zip(paths, labels.cpu().numpy(), preds.cpu().numpy()):
-                with open(csv_file, mode="a", newline="") as file:
-                    filename = os.path.basename(path)
-                    writer = csv.writer(file)
-                    writer.writerow([filename, pred, label])
-
+            if preds != labels:
+                # returns the name of the image classified wrongly
+                print("actual label: " + str(labels) + ", pred: " + str(preds) + ", image: " + loader.dataset.samples[j][0])
+            
+            
     epoch_loss = running_loss / len(loader.dataset)
     accuracy = correct / total
     return epoch_loss, accuracy
 
 
-# Main
-directory = "dl2425_challenge_dataset"
-batch_size = 4
-train_loader, val_loader, val2_loader = data_loader(directory, batch_size)
+# Plot graphs
+def PlotGraphs(csv_file, model_name):
+    # Graphs for visualization
 
-model = ViTClassifier(hidden_classes=3, num_classes=2)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
-model.to(device)
+    # Read the CSV file
+    df = pd.read_csv(csv_file, sep =",")
+    plt.rcParams.update({'font.size': 18}) # updates the font size in all the plots
+    # Plot Train Loss and Val Loss
+    plt.figure(figsize=(10, 6))
+    plt.plot(df["Epoch"], df["Train Loss"], label="Train Loss", marker='o')
+    plt.plot(df["Epoch"], df["Val Loss"], label="Val Loss", marker='o')
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title(model_name + "Train and Validation Loss")
+    plt.legend()
+    plt.grid()
+    plt.savefig(model_name + "_TrainVal_Loss.png")  # Save the plot as an image
+    plt.show()
 
-epochs = 2
-dir_models = "saved_models\\"
-model_name = "VitModel"
-LOAD_MODEL = True # set to True if you want to load a model to continue its training
-START_EPOCH = 0  # the epoch from which you want to continue the training. Leave to 0 if you want to train from scratch
+    # Plot Val Accuracy and Test Accuracy
+    plt.figure(figsize=(10, 6))
+    plt.plot(df["Epoch"], df["Val Accuracy"], label="Val Accuracy", marker='o')
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy (%)")
+    plt.title(model_name + "Validation Accuracy")
+    plt.legend()
+    plt.grid()
+    plt.savefig(model_name + "_ValTest_Accuracy.png")  # Save the plot as an image
+    plt.show()
 
-# Create and write the CSV header before starting the training loop
-csv_file = f"{model_name}_metrics.csv"
-with open(csv_file, mode="w", newline="") as file:
+
+# Performs both training and validation and saves the losses
+def TrainAndValidate(model, train_loader, val_loader, model_metrics, epochs):
+    
+    # Create and write the CSV header before starting the training loop
+    file = open(model_metrics, mode="w", newline="")
     writer = csv.writer(file)
-    writer.writerow(["Epoch", "Train Loss", "Val Loss", "Val Accuracy", "Test Loss", "Test Accuracy"])  # Write header
+    writer.writerow(["Epoch", "Train Loss", "Val Loss", "Val Accuracy"])  # Write header
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.002)
+    
+    for epoch in range(epochs):
+        train_loss = TrainEpoch(model, train_loader, optimizer, criterion, device)
+        torch.save(model.state_dict(), dir_models + model_name + "_epoch_" + str(START_EPOCH + epoch + 1) + ".pt")
+        model.state_dict()
+        
+        val_loss, val_accuracy = ValidateEpoch(model, val_loader, criterion, device)
+        
+        # Write the current epoch's metrics to the CSV file
+        writer.writerow([epoch+1, train_loss, val_loss, val_accuracy])
+        
+        print("Epoch " + str(epoch + 1) + "/" + str(epochs) + ", Train Loss: " + str(round(train_loss, 4)) + "\n" +
+              "Val Loss: " + str(round(val_loss, 4)) + ", Val Accuracy: " + str(round(val_accuracy, 4)))
+    
+    file.close()
 
-# Create CSV file for annotating final predictions of classes
-csv_preds = f"{model_name}_predictions.csv"
+# Runs the model, either with training and validation or printing the results
+def RunModel(directory, batch_size, epochs, dir_models, model_name, LOAD_MODEL, START_EPOCH):
+    train_loader, val_loader, test_loader = DataLoader(directory, batch_size)
+    model = ViTClassifier(hidden_classes=3, num_classes=2)
+    model.to(device)
 
-for epoch in range(epochs):
+    model_metrics = model_name + "_metrics.csv"
+    test_results = "test_results.csv"
 
+    # This will run only if we want to load the weights of our model instead of starting from zero
     if LOAD_MODEL:
         model.load_state_dict(torch.load(dir_models + model_name + "_epoch_" + str(START_EPOCH) + ".pt",
-                                         weights_only=True))  # loads the correct weights in the model
+                                        weights_only=True, map_location=torch.device('cpu')))  # loads the correct weights in the model
         model.eval()
+    
+    TrainAndValidate(model, train_loader, val_loader, model_metrics, epochs)
+    PlotGraphs(model_metrics, model_name)
 
-    train_loss = train_epoch(model, train_loader, optimizer, criterion, device)
-
-    torch.save(model.state_dict(), dir_models + model_name + "_epoch_" + str(START_EPOCH + epoch + 1) + ".pt")
-    model.state_dict()
-
-    val_loss, val_accuracy = validate_epoch(model, val_loader, criterion, device, csv_preds)
-    val2_loss, val2_accuracy = validate_epoch(model, val2_loader, criterion, device, csv_preds)
-
-    # Write the current epoch's metrics to the CSV file
-    with open(csv_file, mode="a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([epoch+1, train_loss, val_loss, val_accuracy, val2_loss, val2_accuracy])
-
-    print(f"Epoch {epoch + 1}/{epochs}, Train Loss: {train_loss:.4f}, "
-          f"Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.4f}, Test Loss: {val2_loss:.4f}, "
-          f"Test Accuracy: {val2_accuracy:}")
+directory = "dl2425_challenge_dataset"
+batch_size = 4
+epochs = 4
+dir_models = "saved_models\\"
+model_name = "VitModel"
+LOAD_MODEL = False # set to True if you want to load a model to continue its training
+START_EPOCH = 1 # the epoch from which you want to continue the training. Leave to 0 if you want to train from scratch
 
 
-# Graphs for visualization
-import pandas as pd
-
-# Read the CSV file
-df = pd.read_csv(csv_file)
-
-# Plot Train Loss and Val Loss
-plt.figure(figsize=(10, 6))
-plt.plot(df["Epoch"], df["Train Loss"], label="Train Loss", marker='o')
-plt.plot(df["Epoch"], df["Val Loss"], label="Val Loss", marker='o')
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.title("Train and Validation Loss")
-plt.legend()
-plt.grid()
-plt.savefig(f"{model_name}_TrainVal_Loss.png")  # Save the plot as an image
-plt.show()
-
-# Plot Val Accuracy and Test Accuracy
-plt.figure(figsize=(10, 6))
-plt.plot(df["Epoch"], df["Val Accuracy"], label="Val Accuracy", marker='o')
-plt.plot(df["Epoch"], df["Test Accuracy"], label="Test Accuracy", marker='o')
-plt.xlabel("Epoch")
-plt.ylabel("Accuracy (%)")
-plt.title("Validation and Test Accuracy")
-plt.legend()
-plt.grid()
-plt.savefig(f"{model_name}_ValTest_Accuracy.png")  # Save the plot as an image
-plt.show()
+RunModel(directory, batch_size, epochs, dir_models, model_name, LOAD_MODEL, START_EPOCH)
